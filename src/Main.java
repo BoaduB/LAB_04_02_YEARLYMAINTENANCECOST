1d28bd0eafeb9371f0e75b30693589913b6202f7
BoaduB <boansibk@mail.uc.edu>
2023-02-02 01:28:03 -0500

    LAB_04_02_YEARLYMAINTENANCECOST

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double fallCost = 500;
+        double winterCost = 403.35;
+        double springCost = 195.95;
+        double summerCost = 301.45;
+
+        double totalCost = 0;
+
+        totalCost = fallCost + winterCost + springCost + summerCost;
+
+        System.out.println("The fall maintenance cost is : $" + fallCost);
+        System.out.println("The winter maintenance cost is : $" + winterCost);
+        System.out.println("The spring maintenance cost is : $" + springCost);
+        System.out.println("The summer maintenance cost is : $" + summerCost);
+        System.out.println("The total yearly maintenance cost is : $" + totalCost);
     }
 }
